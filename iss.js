f3681d26bc1f7b472a12a1427883978d8a80dcc0
Steven <schenn1992@gmail.com>
2020-11-06 01:39:28 +0000

    fix module.exports

--- iss.js
+++ iss.js
@@ -1,4 +1,4 @@
-const { parse } = require('path');
+
 const request = require('request');
 
 const fetchMyIP = function(callback) {
@@ -19,25 +19,22 @@ const fetchMyIP = function(callback) {
 
 const fetchCoordsByIP = function(ip, callback) {
   request(`http://ip-api.com/json/${ip}`, (error, response, body) => {
-  if (error) {
-    callback(error, null);
-    return;
-  }  
+    if (error) {
+      callback(error, null);
+      return;
+    }  
 
-  if (response.statusCode !== 200) {
-    callback(Error(`Status Code ${response.statusCode} when fetching Coordinates for IP: ${body}`), null);
-    return;
-  }
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching Coordinates for IP: ${body}`), null);
+      return;
+    }
   
     let longtitude = JSON.parse(body).lon;
     let latitude = JSON.parse(body).lat;
     const coords = {'latitude': latitude, 'longtitude': longtitude};
     callback(null, coords);
   });
-    
-  
 };
 
 
-module.exports = { fetchMyIP };
-module.exports = { fetchCoordsByIP };
+module.exports = { fetchMyIP, fetchCoordsByIP };
