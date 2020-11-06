6a1cab78d6afdfe4ddae934a6e130d556f373310
Steven <schenn1992@gmail.com>
2020-11-06 01:39:59 +0000

    minor touch up

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const { fetchMyIP } = require('./iss');
 const { fetchCoordsByIP } = require('./iss'); 
 
-// fetchMyIP((error, ip) => {
-//   if (error) {
-//     console.log("It didn't work!" , error);
-//     return;
-//   }
+fetchMyIP((error, ip) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
 
-//   console.log('It worked! Returned IP:' , ip);
+  console.log('It worked! Returned IP:' , ip);
   
-// });
+});
 
 fetchCoordsByIP('165.185.173.254', (error, coords) => {
   if (error) {
