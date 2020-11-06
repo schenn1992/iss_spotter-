f4c91fe9271a389e3753b2d37693c51c8398a69f
Steven <schenn1992@gmail.com>
2020-11-06 01:25:38 +0000

    run fetchCoordsByIP func

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const { fetchMyIP } = require('./iss');
+const { fetchCoordsByIP } = require('./iss'); 
 
-fetchMyIP((error, ip) => {
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned IP:' , ip);
+  
+// });
+
+fetchCoordsByIP('165.185.173.254', (error, coords) => {
   if (error) {
-    console.log("It didn't work!" , error);
+    console.log("It didn't work!", error);
     return;
   }
-
-  console.log('It worked! Returned IP:' , ip);
+  
+  console.log("The coordinates are:", coords);
   
 });
